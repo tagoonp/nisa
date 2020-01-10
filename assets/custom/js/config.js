777884fda4c1c5c66e8d195f0141992b7bcb5954
tagoon <tagoon.p@psu.ac.th>
2020-01-10 15:35:33 +0700

    20200110 15.36

--- assets/custom/js/config.js
+++ assets/custom/js/config.js
@@ -6,7 +6,7 @@ var conf = {
     mail_key: 'cm1pc21lZHBzdUBnbWFpbC5jb20yMDE5LTEwLTIyIDIxOjU4OjU3MTI0LjEyMi40Mi4yNDU=',
 }
 
-<<<<<<< HEAD
+
 var conf = {
     domain: 'https://fxplor.com/nisa/',
     api: 'https://fxplor.com/nisa/controller/',
@@ -16,7 +16,7 @@ var conf = {
     mail_user: 'rmismedpsu@gmail.com',
     mail_key: 'cm1pc21lZHBzdUBnbWFpbC5jb20yMDE5LTEwLTIyIDIxOjU4OjU3MTI0LjEyMi40Mi4yNDU=',
 }
-=======
+// =======
 // var conf = {
 //     domain: 'https://fxplor.com/nisa/',
 //     api: 'https://fxplor.com/nisa/controller/',
@@ -24,7 +24,7 @@ var conf = {
 //     mail_user: 'rmismedpsu@gmail.com',
 //     mail_key: 'cm1pc21lZHBzdUBnbWFpbC5jb20yMDE5LTEwLTIyIDIxOjU4OjU3MTI0LjEyMi40Mi4yNDU=',
 // }
->>>>>>> a18bf235f48ea2018eb41d40da5ce77e78d5ff3f
+// >>>>>>> a18bf235f48ea2018eb41d40da5ce77e78d5ff3f
 
 var current_user = window.localStorage.getItem(conf.prefix + 'uid')
 var current_role = window.localStorage.getItem(conf.prefix + 'role')
