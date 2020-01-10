d3ba9c3de9707714e116a1827ca4a1ec993a5aed
tagoon <tagoon.p@psu.ac.th>
2020-01-10 16:43:12 +0700

    20200110 15.43

--- controller/test1.php
+++ controller/test1.php
@@ -1,4 +1,26 @@
 <?php
 // exec("test_r.R");
-exec("Rscript test_r.R $N");
+// exec("Rscript test_r.R $N");
+
+echo "
+";
+echo "Number values to generate:
+";
+echo "Submit";
+echo ""
+;
+
+if(isset($_GET['N']))
+{
+  $N = $_GET['N'];
+
+  // execute R script from shell
+  // this will save a plot at temp.png to the filesystem
+  exec("Rscript test_r.R $N");
+
+  // return image tag
+  $nocache = rand();
+  echo("");
+}
+
 ?>
