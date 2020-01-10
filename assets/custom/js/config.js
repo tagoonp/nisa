5eff8cfb9567ed6163b9bae3e132d55a95c2c4f3
tagoon <tagoon.p@psu.ac.th>
2020-01-10 15:34:16 +0700

    20200110 15.34

--- assets/custom/js/config.js
+++ assets/custom/js/config.js
@@ -8,15 +8,15 @@ var conf = {
     mail_key: 'cm1pc21lZHBzdUBnbWFpbC5jb20yMDE5LTEwLTIyIDIxOjU4OjU3MTI0LjEyMi40Mi4yNDU=',
 }
 
-// var conf = {
-//     domain: 'https://fxplor.com/nisa/',
-//     api: 'https://fxplor.com/nisa/controller/',
-//     // domain: 'https://postgraduateforum2020.com/',
-//     // api: 'https://postgraduateforum2020.com/controller/',
-//     prefix: 'nsllc_',
-//     mail_user: 'rmismedpsu@gmail.com',
-//     mail_key: 'cm1pc21lZHBzdUBnbWFpbC5jb20yMDE5LTEwLTIyIDIxOjU4OjU3MTI0LjEyMi40Mi4yNDU=',
-// }
+var conf = {
+    domain: 'https://fxplor.com/nisa/',
+    api: 'https://fxplor.com/nisa/controller/',
+    // domain: 'https://postgraduateforum2020.com/',
+    // api: 'https://postgraduateforum2020.com/controller/',
+    prefix: 'nsllc_',
+    mail_user: 'rmismedpsu@gmail.com',
+    mail_key: 'cm1pc21lZHBzdUBnbWFpbC5jb20yMDE5LTEwLTIyIDIxOjU4OjU3MTI0LjEyMi40Mi4yNDU=',
+}
 
 var current_user = window.localStorage.getItem(conf.prefix + 'uid')
 var current_role = window.localStorage.getItem(conf.prefix + 'role')
