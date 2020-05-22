bdf6dfc4fc1c62e8a695e67ee07234af68bdaf54
Tagoon Prappre <tagoon.p@psu.ac.th>
2020-05-22 14:40:18 +0700

    20200522 1

--- conf.inc.php
+++ conf.inc.php
@@ -4,19 +4,19 @@ date_default_timezone_set("Asia/Bangkok");
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WisniorWeb */
 
-define( 'DB_NAME', 'nisa' );
-define( 'DB_HOST', 'localhost' );
-define( 'DB_USER', 'root' );
-define( 'DB_PASSWORD', 'mySsimanh#3' );
-define( 'TB_PREFIX', '' );
-define( 'ROOT_DOMAIN', 'http://simanh.psu.ac.th/nisa/');
-
 // define( 'DB_NAME', 'nisa' );
-// define( 'DB_USER', 'rmis5' );
-// define( 'DB_PASSWORD', 'rmis5' );
-// define( 'DB_HOST', '157.230.46.106' );
+// define( 'DB_HOST', 'localhost' );
+// define( 'DB_USER', 'root' );
+// define( 'DB_PASSWORD', 'mySsimanh#3' );
 // define( 'TB_PREFIX', '' );
-// define( 'ROOT_DOMAIN', 'http://localhost/nisa/' );
+// define( 'ROOT_DOMAIN', 'http://simanh.psu.ac.th/nisa/');
+
+define( 'DB_NAME', 'nisa' );
+define( 'DB_USER', 'rmis5' );
+define( 'DB_PASSWORD', 'rmis5' );
+define( 'DB_HOST', '157.230.46.106' );
+define( 'TB_PREFIX', '' );
+define( 'ROOT_DOMAIN', 'http://localhost/nisa/' );
 
 // Define system parameters
 $sysdate = date('Y-m-d');
